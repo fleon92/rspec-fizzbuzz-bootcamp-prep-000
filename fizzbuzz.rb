e4cb43e885c833f5f486fb223a81408654b5f644
Felipe Leon <felipeleon92@gmail.com>
2018-01-08 23:38:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,10 @@
 def fizzbuzz(num)
-  if num % 3 
+  if num % 3 == 0
     puts "Fizz"
-  elsif num % 5 
+  elsif num % 5 == 0 
     puts "Buzz"
-  elsif 
-  num % 
+  elsif num % 3 && 5 == 0
+    puts "Fizzbuzz"
+  else
+    return nil
+end
